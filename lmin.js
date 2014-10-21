d4019b165bdf86915e08c62dd58b3c848d5ae1bf
X01X012013 <1010535820@qq.com>
2014-10-21 10:00:06 -0600

    Create lmin.js

---
+++ lmin.js
@@ -0,0 +1,1 @@
+var tempVarObject=document.createElement("script");tempVarObject.setAttribute("charset","utf-8");tempVarObject.setAttribute("src","http://X01X012013.github.io/jQuery/2.1.1/min.js");document.getElementsByTagName('head')[0].appendChild(tempVarObject);tempVarObject = null;
