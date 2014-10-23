d48b8f99ea1ae182e7105f7c526e4fbdebe44b86
X01X012013 <1010535820@qq.com>
2014-10-23 13:39:56 -0600

    Delete lmin.js

--- lmin.js
+++
@@ -1,1 +0,0 @@
-var tempVarObject=document.createElement("script");tempVarObject.setAttribute("charset","utf-8");tempVarObject.setAttribute("src","http://X01X012013.github.io/jQuery/last/min.js");document.getElementsByTagName('head')[0].appendChild(tempVarObject);tempVarObject = null;
