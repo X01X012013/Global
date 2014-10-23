d58209dfa78ff4068cb84307eba8cddc1700552d
X01X012013 <1010535820@qq.com>
2014-10-23 10:38:01 -0600

    Update jQueryLast.js

--- jQueryLast.js
+++ jQueryLast.js
@@ -1,9 +1,5 @@
-var tempLoaderFunction = function(s){
-  var t = document.createElement("script");
-  t.setAttribute("charset", "utf-8");
-  t.setAttribute("src", s);
-  document.getElementsByTagName('head')[0].appendChild(t);
-  return null;
-}
-tempLoaderFunction("http://X01X012013.github.io/jQuery/Core-2.1.1/min.js");
-tempLoaderFunction("http://X01X012013.github.io/jQuery/Color-2.1.2/min.js");
+var tempVarObject=document.createElement("script");
+tempVarObject.setAttribute("charset","utf-8");
+tempVarObject.setAttribute("src","http://X01X012013.github.io/jQuery/2.1.1/min.js");
+document.getElementsByTagName('head')[0].appendChild(tempVarObject);
+tempVarObject = null;
