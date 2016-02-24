c41a40d2d91aafc27327aad849a617975e5e7008
X01X012013 <1010535820@qq.com>
2016-02-23 19:03:55 -0700

    Update disqusLoader.js

--- disqusLoader.js
+++ disqusLoader.js
@@ -1,13 +1,20 @@
 //Universal disqus loader
-var disqus_config = function(){};
+var disqus_config = function(){
+  //this.page.url = "";
+  //this.page.identifier = "";
+  //this.page.title = "";
+  this.update = function(url, id, title){
+    this.page.url = url;
+    this.page.identifier = id;
+    this.page.title = title;
+  }
+};
 var disqusLoader = function(name, url, id, title){
   //Set variables
-  disqus_config.prototype.page.url = url;
-  disqus_config.prototype.page.identifier = id;
-  disqus_config.prototype.page.title = title;
+  disqus_config.update(url, id, title);
   //Load embed
   var d = document, s = d.createElement('script');
   s.src = '//' + name + '.disqus.com/embed.js';
   s.setAttribute('data-timestamp', +new Date());
-  (d.head || d.body).appendChild(s);
+  alert("load")//(d.head || d.body).appendChild(s);
 };
