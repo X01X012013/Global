10604a4de2915b86998ff8a3d3c3363824e1295d
X01X012013 <1010535820@qq.com>
2016-08-06 14:39:22 -0600

    update documentation

--- disqusLoader.js
+++ disqusLoader.js
@@ -1,6 +1,13 @@
 "use strict";
 
-//Universal disqus loader
+/**
+ * Universal Disqus loader. 
+ * @function
+ * @param {string} name - The short name of the website. 
+ * @param {string} url - The URL for disqus_config. 
+ * @param {*} id - The identifier for disqus_config. 
+ * @param {string} title - The title for disqus_config. 
+ */
 const disqusLoader = function (name, url, id, title) {
     //Set variables
     window.disqus_config = function () {
