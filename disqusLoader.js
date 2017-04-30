9ec2a332fdaec8c9088117b7d12aa1c1b7f9200a
jspenguin2017 <jspenguin2017@users.noreply.github.com>
2017-04-29 18:01:48 -0600

    Update disqusLoader.js

--- disqusLoader.js
+++ disqusLoader.js
@@ -10,12 +10,13 @@
  * @param {string} title - The title for disqus_config.
  */
 const disqusLoader = function (name, url, id, title) {
-    //Set variables
+    //Set configuration
     window.disqus_config = function () {
         this.page.url = url;
         this.page.identifier = id;
         this.page.title = title;
     };
     //Load embed
-    $("<script>").attr("src", "https://" + name + ".disqus.com/embed.js").attr("data-timestamp", (new Date()).getTime()).appendTo("head");
+    $("<script>").attr("src", "https://" + name + ".disqus.com/embed.js")
+        .attr("data-timestamp", (new Date()).getTime()).appendTo("head");
 };
