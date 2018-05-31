c8e4c7edfaa4fa25ffb34c0ff7cd56a5814fbdf3
Hugo Xu <jspenguin2017@users.noreply.github.com>
2018-05-31 09:23:08 -0600

    Update disqusLoader.js

--- disqusLoader.js
+++ disqusLoader.js
@@ -1,22 +1,25 @@
-//Based on Disqus Universal Code
+/**
+ * Based on Disqus Universal Code.
+ */
 "use strict";
 
 /**
- * Disqus loader, jQuery required.
+ * Disqus loader.
  * @function
  * @param {string} name - The short name of the website.
  * @param {string} url - The URL for disqus_config.
- * @param {*} id - The identifier for disqus_config.
+ * @param {Any} id - The identifier for disqus_config.
  * @param {string} title - The title for disqus_config.
  */
 const disqusLoader = function (name, url, id, title) {
-    //Set configuration
     window.disqus_config = function () {
         this.page.url = url;
         this.page.identifier = id;
         this.page.title = title;
     };
-    //Load embed
-    $("<script>").attr("src", `https://${name}.disqus.com/embed.js`)
-        .attr("data-timestamp", (new Date()).getTime()).appendTo("head");
+
+    const script = document.createElement("script");
+    script.src = "https://" + name + ".disqus.com/embed.js";
+    script.setAttribute("data-timestamp", Date.now());
+    document.head.append(script);
 };
